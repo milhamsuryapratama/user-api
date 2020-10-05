f43fa8abc58756f5333e6630bc5f5ee29639f8b2
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-05 16:10:20 +0700

    fixing main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"user-api/config"
 	"user-api/controllers"
@@ -29,18 +30,39 @@ func main() {
 	entity := controllers.NewUserEntity(repo)
 
 	api := r.Group("/api")
-	handler.UserHandlerFunc(api, entity)
 	// api.POST("/login", Login)
 	// api.Use(authMiddleware)
 	// {
 	// 	handler.UserHandlerFunc(api, entity)
 	// }
-
+	handler.UserHandlerFunc(api, entity)
 	r.Run()
 }
 
+// func authMiddleware(c *gin.Context) {
+// 	tokenString := requestHeader{}
+// 	if err := c.ShouldBindHeader(&tokenString); err != nil {
+// 		c.JSON(200, err)
+// 	}
+// 	token, err := jwt.Parse(tokenString.token, func(token *jwt.Token) (interface{}, error) {
+// 		// Don't forget to validate the alg is what you expect:
+// 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+// 		}
+
+// 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+// 		return []byte("ilham"), nil
+// 	})
+
+// 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+// 		fmt.Println(claims["foo"], claims["nbf"])
+// 	} else {
+// 		fmt.Println(err)
+// 	}
+// }
+
 // Login ...
-func Login(c *gin.Context) {
+// func Login(c *gin.Context) {
 	var user domain.User
 	isLogin := u.Con.Where("username = ? AND password = ?", c.PostForm("username"), c.PostForm("password")).Find(&user)
 
