6cd9e618f2991f289584fcf05f35532b024ff402
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-06 12:44:50 +0700

    fixing connection and add dummy user data

--- config/koneksi.go
+++ config/koneksi.go
@@ -1,19 +1,32 @@
 package config
 
 import (
+	"os"
 	"user-api/domain"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // Connect ...
 func Connect() *gorm.DB {
-	dsn := "root:@tcp(127.0.0.1:3306)/user-api?charset=utf8mb4&parseTime=True&loc=Local"
+	godotenv.Load()
+	dbName := os.Getenv("MYSQL_DB")
+	dsn := "root:@tcp(127.0.0.1:3306)/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// db.Migrator().DropTable(domain.User{})
+	db.Migrator().DropTable(domain.User{})
 	db.AutoMigrate(domain.User{})
+
+	user := domain.User{
+		NamaLengkap: "admin",
+		Username:    "admin",
+		Password:    "admin",
+		Foto:        "makanan.png",
+	}
+
+	db.Create(&user)
 
 	return db
 }
